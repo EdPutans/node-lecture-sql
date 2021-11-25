04ef8f2a60e0785495da6e51170cef12957eff36
Ed 'Catboy' Putans <edputans@gmail.com>
2021-11-25 10:50:55 +0000

    finalized-ish

--- src/app.ts
+++ src/app.ts
@@ -4,7 +4,7 @@ import { Request } from "express";
 
 import Database from 'better-sqlite3';
 
-const db = new Database('doggos.db', { verbose: console.log });
+const db = new Database('src/dogs.db', { verbose: console.log });
 
 const init = db.prepare(`CREATE TABLE IF NOT EXISTS doggos (
     name text NOT NULL,
@@ -22,37 +22,38 @@ app.use(express.json());
 
 const port = 3000;
 
-const getAllStatement = db.prepare(`SELECT * FROM doggos;`);
-app.get("/dogs/", (req, res) => {
-  const dogs = getAllStatement.get();
+const getAll = db.prepare(`SELECT * FROM doggos;`);
+
+app.get("/dogs", (req, res) => {
+  const dogs = getAll.get();
 
   res.send(dogs);
 });
 
-const getDogStatement = db.prepare(`SELECT * FROM doggos WHERE id=?;`)
-
-app.get("/dogs/:id", (req, res) => {
-  const doggo = getDogStatement.get(req.params.id);
+const getDog = db.prepare(`SELECT * FROM doggos WHERE id=?;`)
+app.get("/dogs/:id", (req: DogRequest, res) => {
+  const doggo = getDog.get(req.params.id);
 
   res.send(doggo);
 });
 
-const insertStatement = db.prepare(`INSERT INTO doggos (name, age, size, hasOwner) VALUES (?,?,?,?);`);
+const insertDog = db.prepare(`INSERT INTO doggos (name, age, size, hasOwner) VALUES (?,?,?,?);`);
 app.post("/dogs", async (req: DogRequest, res) => {
 
   const { name, age, size, hasOwner } = req.body;
-  const { lastInsertRowid } = insertStatement.run(name, age, size, hasOwner);
-  const lastInsertedDog = getDogStatement.get(lastInsertRowid);
+  const result = insertDog.run(name, age, size, hasOwner);
+  const lastInsertedDog = getDog.get(result.lastInsertRowid);
 
   res.send(lastInsertedDog);
 });
 
-const updateStatement = db.prepare(`UPDATE doggos SET name=?, age=?, size=?, hasOwner=? WHERE id=?;`);
+const update = db.prepare(`UPDATE doggos SET name=?, age=?, size=?, hasOwner=? WHERE id=?;`);
+
 app.patch("/dogs/:id", (req: DogRequest, res) => {
   const { name, age, hasOwner, size } = req.body;
-  const existingDog = getDogStatement.get(req.params.id);
+  const existingDog = getDog.get(req.params.id);
 
-  updateStatement.run(
+  update.run(
     name ?? existingDog.name,
     age ?? existingDog.age,
     size ?? existingDog.size,
@@ -60,16 +61,19 @@ app.patch("/dogs/:id", (req: DogRequest, res) => {
     req.params.id
   );
 
-  const updatedDog = getDogStatement.get(req.params.id);
+  const updatedDog = getDog.get(req.params.id);
 
   res.send(updatedDog);
 });
 
-const deleteStatement = db.prepare(`DELETE FROM doggos WHERE id=?;`);
+const deleteDog = db.prepare(`DELETE FROM doggos WHERE id=?;`);
 
 app.delete("/dogs/:id", (req: DogRequest, res) => {
-  deleteStatement.run(req.params.id)
-  res.send({ message: "Dog successfully deleted lol" })
+  const dogExists = !!getDog.get(req.params.id);
+  if (!dogExists) return res.status(404).send("Dog not found")
+
+  deleteDog.run(req.params.id)
+  res.send("Dog successfully deleted lol")
 });
 
 app.listen(port, (): void => {
